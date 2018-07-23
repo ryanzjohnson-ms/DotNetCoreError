a796462c022aa4dbaf94248a574d1490d803b59d
Ryan Johnson (CSE) <ryjoh@microsoft.com>
2018-07-23 13:13:16 -0700

    init

---
+++ Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Azure.Devices;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
